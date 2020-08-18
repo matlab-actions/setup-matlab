012296eb51d1a47d46448d94608d6073a367a44f
Johan Pereira <2079700+jwpereira@users.noreply.github.com>
2020-08-18 15:17:09 -0400

    Include parameters in install unit tests

--- src/install.unit.test.ts
+++ src/install.unit.test.ts
@@ -15,6 +15,12 @@ afterEach(() => {
 describe("install procedure", () => {
     let downloadAndRunScriptMock: jest.Mock<any, any>;
 
+    // install() does not perform any logic itself on its parameters. Therefore
+    // they can be held static for these unit tests
+    const platform = "linux";
+    const release = "latest";
+    const doInstall = () => install.install(platform, release);
+
     beforeEach(() => {
         downloadAndRunScriptMock = script.downloadAndRunScript as jest.Mock;
 
@@ -28,14 +34,14 @@ describe("install procedure", () => {
     it("ideally works", async () => {
         downloadAndRunScriptMock.mockResolvedValue(undefined);
 
-        await expect(install.install()).resolves.toBeUndefined();
+        await expect(doInstall()).resolves.toBeUndefined();
         expect(downloadAndRunScriptMock).toHaveBeenCalledTimes(2);
     });
 
     it("rejects when the download fails", async () => {
         downloadAndRunScriptMock.mockRejectedValueOnce(Error("oof"));
 
-        await expect(install.install()).rejects.toBeDefined();
+        await expect(doInstall()).rejects.toBeDefined();
         expect(downloadAndRunScriptMock).toHaveBeenCalledTimes(1);
         expect(core.group).toHaveBeenCalledTimes(1);
     });
@@ -45,7 +51,7 @@ describe("install procedure", () => {
             .mockResolvedValueOnce(undefined)
             .mockRejectedValueOnce(Error("oof"));
 
-        await expect(install.install()).rejects.toBeDefined();
+        await expect(doInstall()).rejects.toBeDefined();
         expect(downloadAndRunScriptMock).toHaveBeenCalledTimes(2);
         expect(core.group).toHaveBeenCalledTimes(2);
     });
