94ed23c311a2b1500dd5153daeb8d512250dc7e9
Johan Pereira <2079700+jwpereira@users.noreply.github.com>
2020-10-19 16:57:25 -0400

    Add docstrings

--- src/script.ts
+++ src/script.ts
@@ -3,6 +3,13 @@
 import * as exec from "@actions/exec";
 import * as toolCache from "@actions/tool-cache";
 
+/**
+ * Download and run a script on the runner.
+ *
+ * @param platform Operating system of the runner (e.g., "win32" or "linux").
+ * @param url URL of the script to run.
+ * @param args Arguments to pass to the script.
+ */
 export async function downloadAndRunScript(platform: string, url: string, args?: string[]) {
     const scriptPath = await toolCache.downloadTool(url);
     const cmd = generateExecCommand(platform, scriptPath);
@@ -14,6 +21,12 @@ export async function downloadAndRunScript(platform: string, url: string, args?:
     }
 }
 
+/**
+ * Generate platform-specific command to run a script.
+ *
+ * @param platform Operating system of the runner (e.g. "win32" or "linux").
+ * @param scriptPath Path to the script (on runner's filesystem).
+ */
 export function generateExecCommand(platform: string, scriptPath: string): string {
     // Run the install script using bash
     let installCmd = `bash ${scriptPath}`;
