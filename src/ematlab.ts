55ea51204c01475fac2e35d752c25ac10d569574
David Buzinski <davidb@mathworks.com>
2022-08-11 08:35:15 -0400

    use native NodeJS.ProcessEnv

--- src/ematlab.ts
+++ src/ematlab.ts
@@ -7,10 +7,6 @@ import * as core from "@actions/core";
 
 export const rootFile = path.join(os.tmpdir(), "ephemeral_matlab_root");
 
-interface ProcessEnv {
-    [key: string]: string | undefined
-}
-
 export function addToPath() {
     let root: string;
     try {
@@ -21,6 +17,6 @@ export function addToPath() {
     core.addPath(path.join(root, "bin"));
 }
 
-export function skipActivationFlag(env: ProcessEnv): string {
+export function skipActivationFlag(env: NodeJS.ProcessEnv): string {
     return (env.MATHWORKS_TOKEN !== undefined && env.MATHWORKS_ACCOUNT !== undefined)? "--skip-activation": "";
 }
