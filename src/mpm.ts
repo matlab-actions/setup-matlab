7bf1c412ec8c353bf15815c6a8317cf19598a796
David Buzinski <davidb@mathworks.com>
2022-08-25 17:19:58 -0400

    borke all tests broke everything?

--- src/mpm.ts
+++ src/mpm.ts
@@ -2,29 +2,35 @@
 
 import properties from "./properties.json";
 import * as script from "./script";
-import * as core from "@actions/core";
 import * as exec from "@actions/exec";
 import * as tc from "@actions/tool-cache";
 import path from "path";
+import * as fs from "fs";
 
 export async function setup(platform: string): Promise<string> {
     const mpmInstallDir: string = process.env.RUNNER_TEMP || script.defaultInstallRoot(platform, "mpm");
     const mpmInstallPath: string = path.join(mpmInstallDir, "mpm");
-    const mpm = await tc.downloadTool(properties.mpmUrl, mpmInstallPath);
-    const exitCode = await exec.exec(`chmod +x ${mpm}`)
-    if (exitCode !== 0) {
-        return Promise.reject(Error("unable to setup mpm"))
-    }
-    return mpm
+    await fs.stat(mpmInstallPath, async (err) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                const mpm = await tc.downloadTool(properties.mpmUrl, mpmInstallPath);
+                const exitCode = await exec.exec(`chmod +x ${mpm}`)
+                if (exitCode !== 0) {
+                    return Promise.reject(Error("unable to setup mpm"))
+                }        
+            }
+        }
+    })
+    return mpmInstallPath
 }
 
 export async function install(mpmPath: string, release: string, products: string[], destination: string = "") {
     if (products.length > 0) {
+
         let mpmArguments: string[] = [
             "install",
             `--release=${release}`,    
         ]
-    
         if (destination) {
             mpmArguments.push(`--destination=${destination}`);
         }
@@ -37,3 +43,4 @@ export async function install(mpmPath: string, release: string, products: string
     }
     return
 }
+  
