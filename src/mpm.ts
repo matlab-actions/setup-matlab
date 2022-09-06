27f14062afc4edca2887b3b83e676186f46da419
David Buzinski <davidb@mathworks.com>
2022-09-06 15:00:39 -0400

    letting tc handle mpm location

--- src/mpm.ts
+++ src/mpm.ts
@@ -1,15 +1,11 @@
 // Copyright 2022 The MathWorks, Inc.
 
 import properties from "./properties.json";
-import * as script from "./script";
 import * as exec from "@actions/exec";
 import * as tc from "@actions/tool-cache";
-import path from "path";
 
 export async function setup(platform: string): Promise<string> {
-    const mpmInstallDir: string = process.env.RUNNER_TEMP || script.defaultInstallRoot(platform, "mpm");
-    const mpmInstallPath: string = path.join(mpmInstallDir, "mpm");
-    const mpm = await tc.downloadTool(properties.mpmUrl, mpmInstallPath);
+    const mpm = await tc.downloadTool(properties.mpmUrl);
     const exitCode = await exec.exec(`chmod +x ${mpm}`)
     if (exitCode !== 0) {
         return Promise.reject(Error("unable to setup mpm"))
