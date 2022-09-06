b5f5aca3b398f99f512bcb7bba9ba2e8487118fe
David Buzinski <davidb@mathworks.com>
2022-09-06 16:54:53 -0400

    trying more tc stuff

--- src/mpm.ts
+++ src/mpm.ts
@@ -27,5 +27,7 @@ export async function install(mpmPath: string, release: string, products: string
     if (exitCode !== 0) {
         return Promise.reject(Error(`Script exited with non-zero code ${exitCode}`));
     }
+
+    tc.cacheDir(destination, "MATLAB", release);
     return
 }
