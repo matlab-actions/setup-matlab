1c0ee4b63b1d95f86a66ffdce59848010069d955
David Buzinski <davidb@mathworks.com>
2022-10-04 13:47:03 -0400

    changing how products are supplied

--- src/mpm.ts
+++ src/mpm.ts
@@ -43,7 +43,7 @@ export async function install(mpmPath: string, release: string, products: string
     if (destination) {
         mpmArguments.push(`--destination=${destination}`);
     }
-    mpmArguments.push(`--products ${products.join(" ")}`);
+    mpmArguments.push(`--products="${products.join(" ")}"`);
 
     const exitCode = await exec.exec(mpmPath, mpmArguments);
     if (exitCode !== 0) {
