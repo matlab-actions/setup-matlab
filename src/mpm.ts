e01e7adfb11dd7519fe98493118653d8b7794847
David Buzinski <davidb@mathworks.com>
2022-10-04 13:28:58 -0400

    slkdlskdfj

--- src/mpm.ts
+++ src/mpm.ts
@@ -23,14 +23,14 @@ export async function setup(platform: string, architecture: string): Promise<str
     }
 
     let mpm: string = await tc.downloadTool(mpmUrl);
-    const exitCode = await exec.exec(`chmod +x ${mpm}`);
-    if (exitCode !== 0) {
-        return Promise.reject(Error("unable to setup mpm"))
-    }
-
     if (platform === "win32") {
        let mpmExtractedPath: string = await tc.extractZip(mpm);
        mpm = path.join(mpmExtractedPath, "bin", "win64",  "mpm.exe");
+    }
+
+    const exitCode = await exec.exec(`chmod +x ${mpm}`);
+    if (exitCode !== 0) {
+        return Promise.reject(Error("unable to setup mpm"))
     }
     return mpm
 }
@@ -45,9 +45,7 @@ export async function install(platform: string, mpmPath: string, release: string
     }
     mpmArguments.push("--products", products.join(" "));
 
-    const cmd: string = script.generateExecCommand(platform, mpmPath);
-
-    const exitCode = await exec.exec(cmd, mpmArguments);
+    const exitCode = await exec.exec(mpmPath, mpmArguments);
     if (exitCode !== 0) {
         return Promise.reject(Error(`Script exited with non-zero code ${exitCode}`));
     }
