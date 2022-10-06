393f98bb65c6db9c931c2dbf20cb34e418993736
David Buzinski <davidb@mathworks.com>
2022-10-06 10:31:20 -0400

    undoing semver test and fixin assert

--- src/matlab.ts
+++ src/matlab.ts
@@ -21,12 +21,11 @@ interface MATLABVersionInfo {
 
 export async function toolcacheLocation(version: Version): Promise<string> {
     let toolpath: string = tc.find("MATLAB", version.semantic);
-    const ver = version.semantic + "-1234567"
     if (toolpath) {
-        core.info(`Found MATLAB ${ver} in cache at ${toolpath}`)
+        core.info(`Found MATLAB ${version.release} in cache at ${toolpath}`)
     } else {
         fs.writeFileSync(".cachematlab", "");
-        toolpath = await tc.cacheFile(".cachematlab", ".cachematlab", "MATLAB", ver)
+        toolpath = await tc.cacheFile(".cachematlab", ".cachematlab", "MATLAB", version.semantic)
         io.rmRF(".cachematlab")
     }
     return toolpath
