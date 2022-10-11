96f024d9f12eeb0b276e26dc788d1fd1791d71df
Release Action <>
2022-10-11 17:14:53 +0000

    Build for 63123b7

---
+++ lib/ematlab.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="node" />
+export declare const rootFile: string;
+export declare function addToPath(): void;
+export declare function skipActivationFlag(env: NodeJS.ProcessEnv): string;
