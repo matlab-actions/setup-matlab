6ef80c9d7c52d8de4243dd05f93a01d592f8edb1
Release Action <>
2022-11-03 18:42:14 +0000

    Build for 6c1f874

---
+++ lib/install.d.ts
@@ -0,0 +1,11 @@
+export default install;
+/**
+ * Set up an instance of MATLAB on the runner.
+ *
+ * First, system dependencies are installed. Then the ephemeral installer script
+ * is invoked.
+ *
+ * @param platform Operating system of the runner (e.g., "win32" or "linux").
+ * @param release Release of MATLAB to be set up (e.g., "latest" or "R2020a").
+ */
+export declare function install(platform: string, release: string, skipActivationFlag: string): Promise<void>;
