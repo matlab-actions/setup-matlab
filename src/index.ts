77f0defe909562d3ed22d74ac9e509e7c06dcda5
David Buzinski <davidb@mathworks.com>
2024-02-08 09:35:36 -0500

    Add upgrade annotation

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ export async function run() {
     const release = core.getInput("release");
     const products = core.getMultilineInput("products");
     const cache = core.getBooleanInput("cache");
+    core.warning("matlab-actions/setup-matlab@v2-beta is no longer supported. Update your workflow to use the most recent version of MATLAB actions (v2).");
     return install.install(platform, architecture, release, products, cache);
 }
 
